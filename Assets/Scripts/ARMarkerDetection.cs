049c86b1ad8f17093e64ef5698e1c870fc43de41
Kazi Jawadul Islam Jishan <kazijawadulislam123@gmail.com>
2023-10-20 23:16:47 +0600

    initial commit

---
+++ Assets/Scripts/ARMarkerDetection.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using TMPro;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARMarkerDetection : MonoBehaviour
+{
+    public TMP_Text counterText;
+
+    private int counter = 0;
+    private ARTrackedImage trackedImage;
+    private bool tapped = false;
+
+    void Start()
+    {
+        ARTrackedImageManager trackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+
+        if (trackedImageManager != null)
+        {
+            trackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+        }
+        else
+        {
+            Debug.LogError("AR Tracked Image Manager not found. Make sure it's in your scene.");
+        }
+    }
+
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.added)
+        {
+            UpdateMarkerDetection(trackedImage);
+        }
+
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            UpdateMarkerDetection(trackedImage);
+        }
+    }
+
+    void UpdateMarkerDetection(ARTrackedImage trackedImage)
+    {
+        if (trackedImage.referenceImage.name == "marker" && tapped)
+        {
+            IncrementCounter();
+        }
+        else
+        {
+            tapped = false;
+        }
+        UpdateCounterText();
+    }
+
+    void IncrementCounter()
+    {
+        counter++;
+    }
+
+    void UpdateCounterText()
+    {
+        counterText.text = counter.ToString();
+    }
+
+    public void ModifyTap()
+    {
+        tapped = true;
+    }
+}
