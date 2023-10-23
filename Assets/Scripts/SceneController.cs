22795285e03998ef032c7ccd9104ec02e9146936
Kazi Jawadul Islam Jishan <kazijawadulislam123@gmail.com>
2023-10-23 23:12:48 +0600

    canvas working fine now

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -6,11 +6,13 @@ public class SceneController : MonoBehaviour
     public void LoadGameScene()
     {
         SceneManager.LoadScene(1); // Load your gameplay scene
+        Debug.Log("Scene 1 loaded");
     }
 
     public void LoadMainMenu()
     {
         SceneManager.LoadScene(0); // Load your main menu scene
+        Debug.Log("Scene 0 loaded");
     }
 
     public void ExitGame()
